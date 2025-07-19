5258810887f13e36b16aeda87b203a1d9c0c360d
lucasdn7 <casludn@gmail.com>
2025-07-18 22:38:27 -0300

    correções deploy

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -164,20 +164,3 @@ export function useAuth() {
   }
   return context;
 }
-
-  };
-
-  return (
-    <AuthContext.Provider value={value}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
-
-export function useAuth() {
-  const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-  return context;
-}
