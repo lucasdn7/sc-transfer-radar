a7eb68a3f74b8b18bbdadb2c552a6db2c35875b8
lucasdn7 <166081638+lucasdn7@users.noreply.github.com>
2026-03-10 11:26:35 -0300

    Implement cache busting for service workers
    Add code to unregister old service workers for cache busting.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+// Limpa service workers antigos (cache busting)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => registration.unregister());
+  });
+}
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
